6cdbb44426bd9d4b333fd5b061aea3718ca3e789
Kayla Nguyen <nguye27k@mtholyoke.edu>
2016-01-23 15:16:14 -0500

    Add map to Restaurant tab.

--- MealMate/www/js/controllers.js
+++ MealMate/www/js/controllers.js
@@ -10,6 +10,127 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  console.log("lsdjkf");
+  $scope.showMap = function(){
+    console.log("Heyyyyy");
+      // google map
+      var map;
+      // variables holding origin latitude and longitude
+      var orig;
+      // info window for marker
+      var infowindow;
+      
+      initSearch();
+      function initSearch() {
+          console.log("Hey I'm here");
+          // initialize map
+          map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: 42.2579438, lng: -72.5785799},
+              zoom: 8,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+          });
+
+          // Create the origin search box and link it to the UI element.
+          var origin = document.getElementById('origin-input');
+          var oriSearchBox = new google.maps.places.SearchBox(origin);
+          // push the oriSearchBox to the map
+          map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin);
+
+          // Bias the SearchBox results towards current map's viewport.
+          map.addListener('bounds_changed', function () {
+              oriSearchBox.setBounds(map.getBounds());
+          });
+
+          // an array of marker on map
+          var markers = [];
+
+          // Listen for the event fired when the user selects a prediction
+          // retrieve more details for that place.
+          oriSearchBox.addListener('places_changed', function () {
+              var oriplaces = oriSearchBox.getPlaces();
+
+              if (oriplaces.length == 0) {
+                  return;
+              }
+
+              // Clear out the old markers.
+              markers.forEach(function (marker) {
+                  marker.setMap(null);
+              });
+              markers = [];
+
+              // For each place, get name and location.
+              var bounds = new google.maps.LatLngBounds();
+              oriplaces.forEach(function (place) {
+                  // Create a marker for each place.
+                  markers.push(new google.maps.Marker({
+                      map: map,
+                      title: place.name,
+                      position: place.geometry.location
+                  }));
+
+                  if (place.geometry.viewport) {
+                      // Only geocodes have viewport.
+                      bounds.union(place.geometry.viewport);
+                  } else {
+                      bounds.extend(place.geometry.location);
+                  }
+                  // set orig to be LatLng type
+                  orig = new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng());
+                  console.log(orig);
+              });
+              // adjust map view
+              map.fitBounds(bounds);
+              map.setZoom(12);
+          });
+
+          if (orig != null){
+            findPOI();
+          }
+      }
+
+      // find restaurant around place
+      function findPOI() {
+          // recenter and zoom on map
+          map.setCenter(dest);
+          map.setZoom(14);
+
+          // initialize info window
+          infowindow = new google.maps.InfoWindow();
+
+          // create new service for search
+          var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch({
+              location: orig,
+              radius: 2000,
+              types: ['restaurant']
+          }, callback);
+      }
+
+      // function called when search
+      function callback(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                  createMarker(results[i]);
+              }
+          }
+      }
+
+      // create marker for place
+      function createMarker(place) {
+          //var placeLoc = place.geometry.location;
+          var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+          });
+
+          // when clicked on marker, show name and rating of place
+          google.maps.event.addListener(marker, 'click', function () {
+              infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + '\nRating: ' + place.rating);
+              infowindow.open(map, this);
+          });
+      }
+  }
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
